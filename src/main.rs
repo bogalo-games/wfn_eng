dc791777d48239f883ca6ced83accb18e9e73756
Cerek Hillen <cerekh@gmail.com>
2018-05-16 10:10:56 -0400

    Buffers

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,11 @@ use vulkano::device::Device;
 use vulkano::buffer::CpuAccessibleBuffer;
 use vulkano::buffer::BufferUsage;
 
+use vulkano::command_buffer::AutoCommandBufferBuilder;
+use vulkano::command_buffer::CommandBuffer;
+
+use vulkano::sync::GpuFuture;
+
 fn main() {
     //
     // Setting up the context.
@@ -40,4 +45,33 @@ fn main() {
     //
     // Messing around
     //
+    let source_content = 0 .. 64;
+    let dest_content = (0 .. 64).map(|_| 0);
+
+    let source = CpuAccessibleBuffer::from_iter(
+        device.clone(),
+        BufferUsage::all(),
+        source_content
+    ).expect("Failed to create src");
+
+    let dest = CpuAccessibleBuffer::from_iter(
+        device.clone(),
+        BufferUsage::all(),
+        dest_content
+    ).expect("Failed to create dst");
+
+    let command_buffer = AutoCommandBufferBuilder::new(
+        device.clone(),
+        queue.family()
+    ).unwrap()
+        .copy_buffer(source.clone(), dest.clone()).unwrap()
+        .build().expect("Could not create command buffer");
+
+    let finished = command_buffer.execute(queue.clone()).unwrap();
+    finished.then_signal_fence_and_flush().unwrap().wait(None).unwrap();
+
+    let src_cont = source.read().unwrap();
+    let dst_cont = dest.read().unwrap();
+
+    assert_eq!(&*src_cont, &*dst_cont);
 }
