b9dd5c1a2ae577388d87686895e7c1b3d79f6cba
Cerek Hillen <cerekh@gmail.com>
2018-05-21 16:50:31 -0400

    Added errors.

---
+++ src/error.cpp
@@ -0,0 +1,51 @@
+#include "error.hpp"
+
+#include <sstream>
+
+namespace wfn_eng {
+    ////
+    // WfnError
+    //
+    // An error used specially for the WFN engine.
+
+    ////
+    // WfnError
+    //
+    // Constructs a WfnError with the following information (in order of
+    // argument list):
+    //   - Module    (namespaced module or class name)
+    //   - Method    (the name of the method that threw an exception)
+    //   - Action    (the section that failed)
+    WfnError::WfnError(std::string module, std::string method, std::string action) {
+
+    }
+
+    ////
+    // std::string module
+    //
+    // Provides a copy of the module string.
+    std::string WfnError::module() const { return _module; }
+
+    ////
+    // std::string method
+    //
+    // Provides a copy of the method string.
+    std::string WfnError::method() const { return _method; }
+
+    ////
+    // std::string action
+    //
+    // Provides a copy of the action string
+    std::string WfnError::action() const { return _action; }
+
+    ////
+    // const char *what()
+    //
+    // Provides a full description of the WfnError (via concatenation of the
+    // module, method, and action)
+    const char *WfnError::what() const noexcept {
+        std::stringstream builder;
+        builder << module() << " - " << method() << " - " << action();
+        return builder.str().c_str();
+    }
+}
---
+++ src/error.hpp
@@ -0,0 +1,55 @@
+#ifndef __WFN_ENG_ERROR_HPP__
+#define __WFN_ENG_ERROR_HPP__
+
+#include <stdexcept>
+#include <string>
+
+namespace wfn_eng {
+    ////
+    // WfnError
+    //
+    // An error used specially for the WFN engine.
+    class WfnError : std::exception {
+        std::string _module;
+        std::string _method;
+        std::string _action;
+
+    public:
+        ////
+        // WfnError
+        //
+        // Constructs a WfnError with the following information (in order of
+        // argument list):
+        //   - Module    (namespaced module or class name)
+        //   - Method    (the name of the method that threw an exception)
+        //   - Action    (the section that failed)
+        WfnError(std::string, std::string, std::string);
+
+        ////
+        // std::string module
+        //
+        // Provides a copy of the module string.
+        std::string module() const;
+
+        ////
+        // std::string method
+        //
+        // Provides a copy of the method string.
+        std::string method() const;
+
+        ////
+        // std::string action
+        //
+        // Provides a copy of the action string
+        std::string action() const;
+
+        ////
+        // const char *what()
+        //
+        // Provides a full description of the WfnError (via concatenation of the
+        // module, method, and action)
+        virtual const char *what() const noexcept;
+    };
+}
+
+#endif
