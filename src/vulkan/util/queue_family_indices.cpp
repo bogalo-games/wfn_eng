c64f683cfa8547cc7f070f06e7e64fa0ff12b69e
Cerek Hillen <cerekh@gmail.com>
2018-05-26 01:44:46 -0400

    Split up Vulkan implementation a bit more/

--- src/vulkan/util/queue_family_indices.cpp
+++ src/vulkan/util/queue_family_indices.cpp
@@ -1,4 +1,4 @@
-#include "../vulkan.hpp"
+#include "../util.hpp"
 
 namespace wfn_eng::vulkan::util {
     ////
@@ -75,62 +75,5 @@ namespace wfn_eng::vulkan::util {
         return graphicsFamily >= 0 &&
                presentationFamily >= 0 &&
                transferFamily >= 0;
-    }
-
-    ////
-    // struct SwapchainSupport
-    //
-    // Utility struct to encapsulate various swapchain capabilities of a
-    // VkPhysicalDevice.
-
-    ////
-    // SwapchainSupport(VkSurfaceKHR, VkPhysicalDevice)
-    //
-    // Queries the swapchain capabilities of a VkSurfaceKHR and a
-    // VkPhysicalDevice directly.
-    SwapchainSupport::SwapchainSupport(VkSurfaceKHR surface, VkPhysicalDevice device) {
-        vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, surface, &capabilities);
-
-        uint32_t formatCount;
-        vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, nullptr);
-        if (formatCount > 0) {
-            formats.resize(formatCount);
-            vkGetPhysicalDeviceSurfaceFormatsKHR(
-                device,
-                surface,
-                &formatCount,
-                formats.data()
-            );
-        }
-
-        uint32_t presentModeCount;
-        vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, nullptr);
-        if (presentModeCount > 0) {
-            presentModes.resize(presentModeCount);
-            vkGetPhysicalDeviceSurfacePresentModesKHR(
-                device,
-                surface,
-                &presentModeCount,
-                presentModes.data()
-            );
-        }
-    }
-
-    ////
-    // SwapchainSupport(Base&, Device&)
-    //
-    // Queries the swapchain capabilities of a VkPhysicalDevice via a
-    // reference to the Base and Device classes (below) that encapsulate
-    // the surface and physical device.
-    SwapchainSupport::SwapchainSupport(Base& base, Device& device) :
-            SwapchainSupport(base.surface(), device.physical()) { }
-
-    ////
-    // bool sufficient
-    //
-    // Returns whether the queried swapchain capabilities are sufficient
-    // to use.
-    bool SwapchainSupport::sufficient() {
-        return formats.size() > 0 && presentModes.size() > 0;
     }
 }
