b89d8e4e393e61bd8b5ad31f98c824aab1e223e9
Cerek Hillen <cerekh@gmail.com>
2018-05-27 19:22:56 -0400

    Remove superfluous function.

--- src/main.cpp
+++ src/main.cpp
@@ -112,26 +112,6 @@ private:
 
     ////
     // Initializing Vulkan components
-
-    void createIndexBuffer() {
-        indexBuffer = new Buffer(
-            indices.size() * sizeof(uint16_t),
-            VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT,
-            VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
-            VK_SHARING_MODE_CONCURRENT
-        );
-
-        Buffer stagingBuffer(
-            indices.size() * sizeof(uint16_t),
-            VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
-            VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
-            VK_SHARING_MODE_CONCURRENT
-        );
-
-        stagingBuffer.copy_from(indices.data());
-        stagingBuffer.copy_to(*indexBuffer);
-    }
-
     void initCommandBuffers() {
         //
         // Creating the command pool
